7b906fcc4e5f5be55e49271d8696930ee8bddc88
hiteshpatel <hpatel14@umbc.edu>
2024-09-27 02:02:28 -0400

    continue button

--- frontend/script.js
+++ frontend/script.js
@@ -3,25 +3,19 @@ $(document).ready(function(){
     $scramble.scramble(3000, 20, "alphabet", true);
 });
 
+$(document).ready(function(){
+  var $scramble = $(".scramble");
+  $scramble.scramble(3000, 20, "alphabet", true);
 
-var tl = gsap.timeline();
-tl.to(".Loading", {
-  opacity: 0,
-  delay: 3.8,
-})
-.to(".loader", {
-    // y: "-100%",
-    opacity: 0,
-    duration: 1,
-    ease: "Expo.easeInOut",
-  })
-  .to(
-    ".loader",
-    {
-      y: "-100%",
-      // opacity:0,
-      duration: 1,
-      // ease: "Expo.easeInOut"
-    },
-    "up"
-  )
+  // Animate the loading bar out and show the button after the loading is complete
+  $(".Loading").on("animationend", function() {
+      // Hide the loading bar smoothly
+      $(this).animate({
+          width: '0%'
+      }, 1000, function() {
+          // This function is called after the loading bar animation is complete
+          // Now display the Continue button
+          $("#continueBtn").fadeIn();
+      });
+  });
+});
