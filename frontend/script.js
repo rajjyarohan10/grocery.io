fbb60442ef750ca670006ac324377dd5b71774aa
hiteshpatel <hpatel14@umbc.edu>
2024-09-28 16:36:21 -0400

    Removed unnecessary comments

--- frontend/script.js
+++ frontend/script.js
@@ -1,28 +1,3 @@
-/*
-$(document).ready(function(){
-    var $scramble = $(".scramble");
-    $scramble.scramble(3000, 20, "alphabet", true);
-});
-
-$(document).ready(function(){
-  var $scramble = $(".scramble");
-  $scramble.scramble(3000, 20, "alphabet", true);
-
-  // Animate the loading bar out and show the button after the loading is complete
-  $(".Loading").on("animationend", function() {
-      // Hide the loading bar smoothly
-      $(this).animate({
-          width: '0%'
-      }, 1000, function() {
-          // This function is called after the loading bar animation is complete
-          // Now display the Continue button
-          $("#continueBtn").fadeIn();
-      });
-  });
-});
-*/
-
-
 $(document).ready(function(){
     var $scramble = $(".scramble");
     $scramble.scramble(3000, 20, "alphabet", true);
